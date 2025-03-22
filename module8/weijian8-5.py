b0a3695e6ef536667b4ccc75928f82768e49174f
David <zhaowj1107@gmail.com>
2025-03-22 16:28:22 -0700

    week8 Homework

--- module8/weijian8-5.py
+++ module8/weijian8-5.py
@@ -1,8 +1,6 @@
-import doctest
-
 """
 File: weijian8-5.py
-Author: Weijian Zhao(David)
+Author: David/Icho/Xiang
 Date: 2025-03-20
 Class: CS_5002, Spring_2025
 Description: 
@@ -45,6 +43,16 @@ def bubble_sort(list_n):
 
 
 def insert_sort(list_n):
+    """
+    Sort the list using insert sort algorithm.
+    args:
+    list_n: List of elements to be sorted.
+
+    >>> insert_sort([5, 4, 3, 2, 1])
+    [1, 2, 3, 4, 5]
+    >>> insert_sort([3, 1, 4, 1, 5])
+    [1, 1, 3, 4, 5]
+    """
     length = len(list_n)
     for idx_i in range(length):
         while idx_i != 0:
@@ -55,15 +63,76 @@ def insert_sort(list_n):
     return list_n
 
 
-def merge_sort(list_n):
+def split(list_n):
+    """
+    Split the list into two parts.
+    args:
+    list_n: List of elements to be split.
     
+    >>> split([1,2,3,4,5,6,7,8])
+    ([1, 2, 3, 4], [5, 6, 7, 8])
+    >>> split([1,2,3,4,5,6,7])
+    ([1, 2, 3], [4, 5, 6, 7])
+    """
+    mid = 0 + (0 + len(list_n))//2
+    return list_n[:mid], list_n[mid:]
+
+def merge(list1,list2):
+    """
+    Merge two sorted lists into one sorted list.
+    the args are two sorted lists.
+    args:
+    list1: List of elements to be merged."
+    """
+    list3 = []
+    i, j = 0, 0
+    while len(list3) != len(list1) + len(list2):
+        if list1[i] >= list2[j]:
+            list3.append(list2[j])
+            if j < len(list2) - 1:
+                j += 1
+            else:
+                for x in list1[i:]:
+                    list3.append(x)
+        elif list1[i] < list2[j]:
+            list3.append(list1[i])
+            if i < len(list1) - 1:
+                i += 1
+            else:
+                for y in list2[j:]:
+                    list3.append(y)
+        print(list3)
+    return list3
+
+
+def merge_sort(list_n):
+    """
+    Sort the list using merge sort algorithm.
+    use recursion to split the list into two parts and merge them with merge function.
+    args:
+    list_n: List of elements to be sorted.
+    """
+    if len(list_n) == 1:
+        print(list_n)
+        return list_n
+    else:
+        list_left, list_right = split(list_n)
+        return merge(merge_sort(list_left), merge_sort(list_right))
+
 
 if __name__ == "__main__":
-    # doctest.testmod()
-
     list_a = [5, 4, 3, 2, 1]
     print("Here is the bubble sort.")
     bubble_sort(list_a)
     list_a = [5, 4, 3, 2, 1]
     print("\nHere is the bubble sort.")
     insert_sort(list_a)
+
+    list_n = [100,3,5,7,2,4,6,8,1]
+    merge_sort(list_n)
+    
+    # list1 = [1,3,5,7]
+    # list2 = [2,4,6,8]
+    # merge(list1,list2)
+
+
